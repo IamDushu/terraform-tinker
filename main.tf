dbf681acb925fe1f7238501d92035d2e93ba47bd
IamDushu <hey.dushyanth@gmail.com>
2024-09-13 15:35:27 +0530

    Associating local ssh key pair

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "ami_id" {}
 variable "instance_type" {}
+variable "my_public_key" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -117,6 +119,12 @@ resource "aws_security_group" "myapp-sg" {
 #   value = data.aws_ami.latest-amazon-linux-image
 # }
 
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  #   public_key = var.my_public_key
+  public_key = file(var.public_key_location)
+}
+
 resource "aws_instance" "myapp-server" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -126,7 +134,7 @@ resource "aws_instance" "myapp-server" {
   availability_zone      = var.avail_zone
 
   associate_public_ip_address = true
-  key_name                    = "server-key-pair"
+  key_name                    = aws_key_pair.ssh-key.key_name
 
   tags = {
     Name : "${var.env_prefix}-server"
