84ba56d468e1b36464c52e17e880f37edc6242ec
IamDushu <hey.dushyanth@gmail.com>
2024-09-15 19:52:39 +0530

    Creating Module files

--- main.tf
+++ main.tf
@@ -9,48 +9,12 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
+module "myapp-subnet" {
+  source            = "./modules/subnet"
   vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name : "${var.env_prefix}-subnet-1"
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name : "${var.env_prefix}-igw"
-  }
-}
-
-# configuring default route table; No subnet association needed here
-# resource "aws_default_route_table" "main-rtb" {
-#   default_route_table_id = aws_vpc.myapp-vpc.main_route_table_id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.myapp-igw.id
-#   }
-#   tags = {
-#     Name : "${var.env_prefix}-main-rtb"
-#   }
-# }
-
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name : "${var.env_prefix}-rtb"
-  }
-}
-
-resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone        = var.avail_zone
+  env_prefix        = var.env_prefix
 }
 
 # Configuring Default SG
